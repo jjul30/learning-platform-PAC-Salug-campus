52cae466c351ac9baa7ef1cd7f9f7a64c690b7b5
jjul30 <70503537+jjul30@users.noreply.github.com>
2025-07-09 17:14:09 +0800

    run.py

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+from flask import Flask, render_template
 
-from app import create_app
+app = Flask(__name__)
 
-app = create_app()
+@app.route('/')
+def home():
+    return "Welcome to PAC Salug Campus Learning Platform!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
